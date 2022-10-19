6e832c733955d2fa581dfbd309dfe3eb59e1c531
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-10-19 17:55:05 +0530

    completed the backend

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
 const app = require('express')()
-const {createServer} = require('http')
-const httpServer  = createServer(app)
+const { createServer } = require('http')
+const httpServer = createServer(app)
 
-const {Server} = require('socket.io')
-const io = new Server(httpServer, {
-    // ...
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: "*"
+  },
 })
 
+const port = 8000
+io.on('connection', (socket) => {
+  console.log('What is a socket?', socket)
+  console.log('Socket is active to be connected...')
 
-const port = 3000
-io.on('connection' , (socket)=>{
-    console.log('What is a socket?', socket)
-    console.log('Socket is active to be connected...')
-
-    //connection, disconnect, ...some user defined strings 
-    socket.on('chat', (payload)=>{
-        console.log('What is a payload?', payload)
-        //responding to this event
-        io.emit('chat', payload)
-    })
+  //connection, disconnect, ...some user defined strings
+  socket.on('chat', (payload) => {
+    console.log('What is a payload?', payload)
+    //responding to this event
+    io.emit('chat', payload)
+  })
 })
 
-httpServer.listen(port, ()=>{
-    console.log(`Server is listening at port ${port}...`)
+httpServer.listen(port, () => {
+  console.log(`Server is listening at port ${port}...`)
 })
